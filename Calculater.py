e9edfd413199763ab86b42dfeabf71b8854306a8
Pranjal Sharma <pranjal.software.engineer@gmail.com>
2022-12-15 17:30:43 +0530

    First Module

---
+++ Calculater.py
@@ -0,0 +1,16 @@
+def add(a,b):
+    print("{}+{}".format(a,b))
+    return a+b
+
+def sub(a,b):
+    print("{}-{}".format(a,b))
+    return a-b
+
+def mul(a,b):
+    print("{}*{}".format(a,b))
+    return a*b
+
+
+def div(a,b):
+    print("{}/{}".format(a,b))
+    return a//b
---
+++ math.py
@@ -0,0 +1,9 @@
+import Calculater as Cal
+
+print(Cal.add(9,9))
+
+print(Cal.sub(9,9))
+
+print(Cal.mul(9, 9))
+
+print(Cal.div(9, 9))
