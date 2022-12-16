ab92dc03dfa3f39a51829a1c2e5c7acebdc0ae4d
Pranjal Sharma <pranjal.software.engineer@gmail.com>
2022-12-16 07:57:47 +0530

    package

---
+++ math2.py
@@ -0,0 +1,6 @@
+from mathfun import *
+
+print(simple.add(9,9))
+
+print(complex.square(9))
+print(globals())
---
+++ mathfun/complex.py
@@ -0,0 +1,9 @@
+def square(x):
+    print('The square of {}'.format(x))
+    return x**2
+
+
+def cube(x):
+    print('The cube of {}'.format(x))
+    return x**3
+
